f0d84bc8a68ec1cf500f2f9a9ead9399a2fe1300
Nekhorosheva <nekhorosheva.ann@gmail.com>
2023-06-26 15:35:01 -0700

    style button in movies section

--- src/components/Movies/MoviesCardList/MoviesCardList.js
+++ src/components/Movies/MoviesCardList/MoviesCardList.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
 import MoviesCard from "../MoviesCard/MoviesCard.js";
 
 function MoviesCardList({ cardList, typeCardBtn }) {
+  const [showAll, setShowAll] = useState(false);
+
+  const visibleCards = showAll ? cardList : cardList.slice(0, 12); // Отображать все карточки или только первые 12
+
+  const handleShowMore = () => {
+    setShowAll(true);
+  };
+
   return (
-    <section className="movies-card">
-      <ul className="movies-card__list">
-        {cardList.map((card) => (
+    <section className="cards">
+      <ul className="cards__list">
+        {visibleCards.map((card) => (
           <MoviesCard
             key={card.movieId}
             movieId={card.movieId}
@@ -15,8 +24,12 @@ function MoviesCardList({ cardList, typeCardBtn }) {
           />
         ))}
       </ul>
-      {cardList.length > 2 && (
-        <button className="movies-card__more-btn">Ещё</button>
+      {!showAll && cardList.length > 12 && (
+        <div className="cards__block-more">
+          <button className="cards__btn-more" onClick={handleShowMore}>
+            Ещё
+          </button>
+        </div>
       )}
     </section>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -61,9 +61,282 @@ export const cardList = [
     nameRU: "Аватар",
     nameEN: "Avatar",
   },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    trailerLink:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    thumbnail:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    movieId: 1122,
+    nameRU: "Аватар Аватар Аватар Аватар Аватар Аватар",
+    nameEN: "Avatar Avatar Avatar Avatar Avatar Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    trailerLink:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    thumbnail:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    movieId: 1123,
+    nameRU:
+      "Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    trailerLink:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    thumbnail:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    movieId: 1124,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    trailerLink: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    thumbnail: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpgg",
+    movieId: 1125,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    trailerLink:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    thumbnail:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    movieId: 1122,
+    nameRU: "Аватар Аватар Аватар Аватар Аватар Аватар",
+    nameEN: "Avatar Avatar Avatar Avatar Avatar Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    trailerLink:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    thumbnail:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    movieId: 1123,
+    nameRU:
+      "Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    trailerLink:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    thumbnail:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    movieId: 1124,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    trailerLink: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    thumbnail: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpgg",
+    movieId: 1125,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    trailerLink:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    thumbnail:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    movieId: 1122,
+    nameRU: "Аватар Аватар Аватар Аватар Аватар Аватар",
+    nameEN: "Avatar Avatar Avatar Avatar Avatar Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    trailerLink:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    thumbnail:
+      "https://i.artfile.me/wallpaper/08-10-2011/1600x1200/knight-and-day-kino-filmy-kruz-diaz-ryca-579928.jpg",
+    movieId: 1123,
+    nameRU:
+      "Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    trailerLink:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    thumbnail:
+      "https://mebellka.ru/wp-content/uploads/0/0/9/009a021709446a5ab8f359c264a8d81f.jpeg",
+    movieId: 1124,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    trailerLink: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    thumbnail: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpgg",
+    movieId: 1125,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
 ];
 
 export const saveCardList = [
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    trailerLink: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    thumbnail: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpgg",
+    movieId: 1124,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    trailerLink:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    thumbnail:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    movieId: 1125,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    trailerLink: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    thumbnail: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpgg",
+    movieId: 1124,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    trailerLink:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    thumbnail:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    movieId: 1125,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    trailerLink: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpg",
+    thumbnail: "https://greatgamer.ru/images/news/05.2022/01/news_11924.jpgg",
+    movieId: 1124,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
+  {
+    country: "USA",
+    director: "James Cameron",
+    duration: 200,
+    year: "2009",
+    description: "epic science fiction film",
+    image:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    trailerLink:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    thumbnail:
+      "https://mobimg.b-cdn.net/v3/fetch/ba/ba01252af8be5dd84eb1e68c2fa93d90.jpeg",
+    movieId: 1125,
+    nameRU: "Аватар",
+    nameEN: "Avatar",
+  },
   {
     country: "USA",
     director: "James Cameron",
